8aa41f4e1d4e1ee9bc3143339fdcbcf99db1b447
aivandag <aivan.dev7@gmail.com>
2025-05-10 18:50:29 +0000

    todofuncional

--- src/js/components/Todo.jsx
+++ src/js/components/Todo.jsx
@@ -1,34 +1,83 @@
 import React, { useState } from "react";
+import "../../styles/Todo.css";
 
-
-
+const inicialEstadoTarea = {
+    label: "",
+    is_done: false
+}
 const Todo = () => {
-    const [tarea, setTarea] = useState("");
+    const [tarea, setTarea] = useState(inicialEstadoTarea);
+    const [listaTarea, setListaTarea] = useState([]);
+
+    const handleChange = (evento) => {
+        setTarea({
+            is_done: false,
+            label: evento.target.value,
+        });
+    };
+
+    const handleKeyDown = (evento) => {
+        if (evento.key === "Enter" && tarea.label.trim() !== "") {
+            evento.preventDefault();
+            setListaTarea([...listaTarea, tarea]);
+            console.log("lista actualizada", [...listaTarea, tarea]);
+            setTarea(inicialEstadoTarea);
+        }
+    };
 
 
     return (
 
         <div className="container">
             <h1 className="titulo"> To Dos</h1>
-            <form className="border p-3" > 
-            <input 
-            className="input-tarea" 
-            type="text" 
-            placeholder="What needs to be done?"
-            />
-</form>
+            <form className="border" >
+                <input
+                    className="input-tarea"
+                    type="text"
+                    placeholder="What needs to be done?"
+                    name="label"
+                    value={tarea.label}
+                    onChange={handleChange}
+                    onKeyDown={handleKeyDown}
+                />
+            </form>
 
+            <ul className="lista">
+                {listaTarea.length === 0 ? (
+                    <li className="lista-item">no tienes tareas pendientes, agrega 1 tarea</li>
+                ) : (
 
-
-
-
-        </div>
+                    listaTarea.map((item, index) => (
+                        < li
+                            key={index}
+                            className="lista-item"
+                        >
+                            {item.label}
+                            <span
+                                className="text-danger"
+                                style={{ cursor: "pointer" }}
+                                onClick={() =>
+                                    setListaTarea(listaTarea.filter((_, i) => i !== index))
+               }
+               >
+🗑
+                        </span>
+        </li>
+            ))
+)}
+        </ul >
+        </div >
 
     );
 
 };
 
 export default Todo;
+
+
+
+
+
 
 // import React, { useState } from "react";
 // import "../../styles/semaforo.css";
